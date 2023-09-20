5b22ed0a540e3b556d743dad33a4aa63631aa1e6
kh4l1d64 <khalid.m@esmagico.in>
2023-09-20 12:25:23 +0530

    Defined prisma schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
+import { ProviderModule } from './provider/provider.module';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AdminModule, ProviderModule, UserModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
